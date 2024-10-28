ed31bd863652d33c129ad3a06e9704a7d8e3d313
luisbaquiax <luis.baquiax1995@gmail.com>
2024-10-27 20:38:00 -0600

    se ha dockerizado las aplicaciones

--- api-files/dist/data/Coneccion.js
+++ api-files/dist/data/Coneccion.js
@@ -4,6 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
-const url = "mongodb://localhost:27017/files";
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const url = "mongodb://mongodb:27017/files";
+//const url = "mongodb://localhost:27017/files";
 const coneccion = mongoose_1.default.connect(url);
 exports.default = coneccion;
--- api-files/src/data/Coneccion.ts
+++ api-files/src/data/Coneccion.ts
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
+import env from 'dotenv';
+env.config();
 
-const url = "mongodb://localhost:27017/files";
+const url = "mongodb://mongodb:27017/files";
+//const url = "mongodb://localhost:27017/files";
 
 const coneccion = mongoose.connect(url);
 
