60e42fc064e825426caf8be2f4b532a872d1edf4
luisbaquiax <luis.baquiax1995@gmail.com>
2024-10-31 13:05:42 -0600

    update readme

--- api-files/src/data/Coneccion.ts
+++ api-files/src/data/Coneccion.ts
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 import env from 'dotenv';
 env.config();
 
-//const url = "mongodb://mongodb:27017/files";
-const url = "mongodb://localhost:27017/files";
+const url = "mongodb://mongodb:27017/files";
+//const url = "mongodb://localhost:27017/files";
 
 const coneccion = mongoose.connect(url);
 
